920ba9bec04a3d8eebf42132849892b2bf17da57
Nayem <nayemishan377@gmail.com>
2022-01-23 16:22:57 +0600

    Scroll to up button added

--- resources/js/jquery.js
+++ resources/js/jquery.js
@@ -4,3 +4,17 @@ function openNav(){
 function closeNav(){
     document.getElementById("my-nav").style.width = "0%";
 }
+
+// scrollToUp
+const btnScrollToTop = document.querySelector("scrollToTop");
+
+scrollToTop.addEventListener("click", function(){
+    // windowbtnScrollToTopscrollTo(0,0);
+
+    window.scrollTo({
+       top: 0,
+       left: 0, 
+       behavior: "smooth"
+    });
+});
+
