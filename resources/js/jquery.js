4cf1e470f55cd47341b291da03b0b8423ac5cf61
Nayem <nayemishan377@gmail.com>
2022-02-04 19:15:28 +0600

    Register page validatin success

--- resources/js/jquery.js
+++ resources/js/jquery.js
@@ -51,3 +51,46 @@ $('#lForm').validate({
         }
     }
 });
+
+//Login form validation
+$('#cFrom').validate({
+    rules: {
+        cusername:{
+            required: true,
+            minlength: 4,
+            maxlength: 20,
+
+        },
+        cemail: {
+            required: true,
+            
+        },
+        cpassword: {
+            required: true,
+            minlength: 5,
+        },
+        cpasswordConfirm: {
+            required: true,
+            equalTo: '#cpassword',
+        }
+    },
+    messages: {
+        cusername: {
+            required: "Username cannot be blank",
+            minlength: "username must be at least 4 characters",
+            maxlength : "username must be at least under 20 characters"
+        },
+        cemail: {
+            required: "Email cannot be blank",
+        },
+        cpassword: {
+            required: "Password cannot be blank",
+            minlength: "password must be at least 5 characters",
+        },
+        cpasswordConfirm: {
+            equalTo: "Password doesn't match",
+        }
+    }
+
+
+});
